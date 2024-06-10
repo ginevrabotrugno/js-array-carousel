949a82da4790309d2e32e15d01d66c3e44169060
ginevrabotrugno <ginevrabotrugno.gb@gmail.com>
2024-06-10 17:40:22 +0200

    feat: add first program

---
+++ js/main.js
@@ -0,0 +1,30 @@
+// Adesso rimuoviamo tutto il markup statico e inseriamo tutte le immagini dinamicamente servendoci dell’array fornito e un semplice ciclo for che concatena un template literal. Tutte le immagini saranno nascoste, tranne la prima, che avrà una classe specifica che la renderà visibile. Al termine di questa fase ci ritroveremo con lo stesso slider stilato nella milestone 1, ma costruito dinamicamente attraverso JavaScript.
+
+// creo array con le immagini
+const imageList = [
+    "img/01.webp", 
+    "img/02.webp", 
+    "img/03.webp", 
+    "img/04.webp", 
+    "img/05.webp"
+];
+
+// seleziono dall'html l'elemento contenitore
+const carousel = document.querySelector(".carousel");
+
+// definisco in una variabile gli elementi della lista 
+for (let i = 0; i < imageList.length; i++) {
+    const image = imageList[i];
+    console.log(image);
+    
+    // creo l'elemento da inserire nell'html 
+    let item = `
+            <div class="item">
+                <img src="${image}" alt="image">
+            </div>`;
+
+    // inserisco l'elemento nell'html
+    carousel.innerHTML += item;
+}
+
+// Al click dell’utente sulle frecce, il programma cambierà l’immagine attiva, che quindi verrà visualizzata al posto della precedente.
